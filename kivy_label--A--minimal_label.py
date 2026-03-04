dfa72dd4ac287603203425314d63982051e3937f
JonPlusPlus <109053054+JonPlusPlus@users.noreply.github.com>
2026-03-04 22:49:47 +0000

    Enhance code documentation with comments
    Add comments to clarify class and method functionality.

--- kivy_label--A--minimal_label.py
+++ kivy_label--A--minimal_label.py
@@ -4,10 +4,12 @@ from kivy.uix.label import Label
 
 kivy.require('1.11.1')
 
-
+# class A(App) define the app class (inherits App).
 class A(App):
+    # build() returns the widget to display (a Label with text).
     def build(selfself):
         return Label(text='Hello Kivy')
 
 
+# A().run() create and run the app; Kivy opens a window and shows the label.
 A().run()
