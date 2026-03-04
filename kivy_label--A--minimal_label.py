fbc65b8a7d0126f3cc3b0a2a7f5d2f9a63ae352b
JonPlusPlus <109053054+JonPlusPlus@users.noreply.github.com>
2026-03-04 22:24:58 +0000

    Fix indentation and formatting issues in minimal_label.py

--- kivy_label--A--minimal_label.py
+++ kivy_label--A--minimal_label.py
@@ -2,12 +2,12 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 
-
 kivy.require('1.11.1')
+
 
 class A(App):
     def build(selfself):
         return Label(text='Hello Kivy')
 
-A().run()
 
+A().run()
