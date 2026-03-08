2d9c06d8f4744d8f6e911f259ec47aa350d072f5
JonPlusPlus <jonathanfosterpayne@proton.me>
2026-03-08 20:54:19 +0000

    First Kiv CheckBox script.

---
+++ 6. Checkboxes/A. single_checkbox.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+
+class BasicCheckBox(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Label(text='Accept Terms', pos=(20,400)): creates the descriptive label.
+        self.lbl = Label(text='Accept Terms', pos=(20, 400))
+        self.add_widget(self.lbl)
+        # CheckBox(pos=(150,400), active=False): creates an unchecked checkbox.
+        self.chk = CheckBox(pos=(150, 400), active=False)
+        # self.add_widget(...): adds widgets to the root widget.
+        self.add_widget(self.chk)
+
+
+class CheckBoxApp(App):
+    def build(self):
+        return BasicCheckBox()
+
+
+if __name__ == '__main__':
+    CheckBoxApp().run()
